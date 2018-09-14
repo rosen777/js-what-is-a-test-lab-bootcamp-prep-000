5a0a24d72c6a8c6a608c7579fe7e7de51d23f90f
Rosen Toshev <toshev@usc.edu>
2018-09-14 08:40:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var name = "Susan"
 var height = 39
 var message = `Name is ${name}. Height is ${height} inches tall`
 
-module.exports = {name, height, message}https://github.com/rosen777/js-what-is-a-test-lab-bootcamp-prep-000
+module.exports = {name, height, message}
